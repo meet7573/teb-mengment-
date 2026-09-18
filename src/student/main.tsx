cccf1ad591ba1da00932a61f0e4cedc829ba0dc2
meet devani <105169060+meet7573@users.noreply.github.com>
2026-09-18 16:20:18 +0530

    Revert UI-only CSS import change

--- src/student/main.tsx
+++ src/student/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { StudentTabletApp } from './StudentTabletApp';
-import './student.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
